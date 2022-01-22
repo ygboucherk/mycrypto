16ccdab11fcfe3ba66ceb7edf9164edbb4d0e65f
ygboucherk <ygboucherk@gmail.com>
2022-01-22 14:09:10 +0100

    Create miningClient.py

---
+++ miningClient.py
@@ -0,0 +1,16 @@
+from myCrypto_client import BeaconChain, Beacon, GenesisBeacon
+
+class MiningClient {
+    def __init__(self, NodeAddr):
+        # self.chain = BeaconChain()
+        self.node = NodeAddr
+        self.difficulty = 1
+        self.target = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        self.lastBlock = ""
+    
+    def pullMiningInfo(self):
+        info = requests.get(f"{self.node}/chain/miningInfo").json().get("result")
+        self.target = info["target"]
+        self.difficulty = info["difficulty"]
+        self.lastBlock = info["lastBlockHash"]
+    
